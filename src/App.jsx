e94cc1d47f8f42048a5674a0db7c7a9e2fe1f988
adritorafsan <adritorafsan@gmail.com>
2024-01-25 15:37:07 +0600

    shit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Feature from './components/Feature'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
 
@@ -9,6 +10,7 @@ function App() {
     <>
   <Navbar />
   <Home />
+  <Feature />
     </>
   )
 }
